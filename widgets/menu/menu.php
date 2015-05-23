a03ac8b3fc69d260f9481282222a8383b231f4b6
Laca <perjeslaszlo@gmail.com>
2015-05-23 19:11:15 +0200

    If the content disabled on content table, the menu has been hide on the top.

--- widgets/menu/menu.php
+++ widgets/menu/menu.php
@@ -14,12 +14,23 @@ function mwidget_menu( $menu_id, $class = null, $id = null ) {
 
 		if ( $menu ) {
 				$pages = $menu->get_pages();
+			//print_r(MObject::get( 'content', intval( 245 ) )); die();
 
 				if ( sizeof( $pages ) > 0 ) {
+						$ContentEnabled = NULL;
 						?>
 								<ul<?php echo $class; ?><?php echo $id; ?>>
 										<?php foreach ( $pages as $page ): ?>
-												<?PHP if (($page->is_enabled()) == 1) { ?>
+											<?PHP $TheLink = $page->get_url();
+												$ContentID = substr(strrchr($TheLink, '='), 1);
+												if (($ContentID) > 0) {
+													$content = MObject::get( 'content', intval( $ContentID ));
+													$ContentEnabled = ($content->is_enabled());
+												} else {
+													$ContentEnabled = 1;
+												}
+											?>
+												<?PHP if ((($page->is_enabled()) == 1) && ($ContentEnabled == 1)) { ?>
 														<li>
 																<a href="<?php MPut::_link( $page->get_url() ); ?>" title="<?php MPut::_html_attr( $page->get_title() ); ?>">
 																		<?php MPut::_html( $page->get_title() ); ?>
