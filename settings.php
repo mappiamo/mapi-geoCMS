67862f43ae1badb75d36bbe22cc7b893514ffb79
Bertalan Ivan <bertalan.ivan@gmail.com>
2015-06-08 14:22:05 +0200

    Preferences  update

--- settings.php
+++ settings.php
@@ -21,7 +21,24 @@ class MSettings{
 	    static $reg_email = '';
 	    static $reg_email_user = '';
 	    static $reg_email_pass = '';
-	    static $reg_email_host = '';
+	    static $reg_email_host = ''; 
+	    
+			public function __construct() {
+				$contents = ORM::for_table( 'preferences' )->find_array();
+				//print_r($contents);
+				$coords = array();
+				foreach ($contents as $pkey => $pvalue) {
+					if ($pvalue['name'] == 'DefaultLatitude') {
+						$this::$coords['lat'] = $pvalue['value'];
+					}
+					if ($pvalue['name'] == 'DefaultLongitude') {
+						$this::$coords['lng'] = $pvalue['value'];
+					}
+					if ($pvalue['name'] == 'location') {
+						$this::$location = $pvalue['value'];
+					}
+				}
+			}
     
 }
 
