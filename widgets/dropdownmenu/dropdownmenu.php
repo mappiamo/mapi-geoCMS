17c3f395c9ba49f61e2091b36a73b5859f8c8425
Laca <perjeslaszlo@gmail.com>
2015-09-02 17:21:10 +0200

    Menu system reads URL from server

--- widgets/dropdownmenu/dropdownmenu.php
+++ widgets/dropdownmenu/dropdownmenu.php
@@ -21,10 +21,21 @@ function mwidget_dropdownmenu( $menu_item ) {
 				<?php
 				if ( sizeof( $pages ) > 0 ) {
 						foreach ( $pages as $page ) {
-								?>
+							$TheLink = $page->get_url();
+							if (strrpos($TheLink, '/', 8)) {
+								$TheLinkRoot = substr($TheLink, 0, strrpos($TheLink, '/', 8));
+							} else {
+								$TheLinkRoot = $TheLink;
+							}
+							$TheServerRoot = str_replace("/manager", "", rtrim(((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on')) ? 'https://' : 'http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']), '/\\'));
+							$TheRealLink = str_replace($TheLinkRoot, $TheServerRoot, $TheLink);
+
+							if ($page->page_on_blank() == TRUE) { $TargetString = '_blank'; } else { $TargetString = '_self'; }
+
+							?>
 										<div style="position: relative; margin:6px; text-align:left;">
 												<li style="white-space:nowrap;">
-												<a href="<?php MPut::_link( $page->get_url() ); ?>" title="<?php MPut::_html_attr( $page->get_title() ); ?>" style="margin: 6px 0; font-size: 13px;">
+												<a href="<?php echo $TheRealLink; //MPut::_link( $page->get_url() ); ?>" target="<?PHP echo $TargetString; ?>" title="<?php MPut::_html_attr( $page->get_title() ); ?>" style="margin: 6px 0; font-size: 13px;">
 														<?php //MPut::_html( $page->get_title() ); ?>
 														<?php __( $page->get_title() ); ?>
 												</a>
