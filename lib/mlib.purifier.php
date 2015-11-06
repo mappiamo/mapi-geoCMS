45ea2289de3a5bd0d067cbdbab71f313c0381712
Laca <perjeslaszlo@gmail.com>
2015-11-06 12:06:48 +0100

    HTML validation rules changed

--- lib/mlib.purifier.php
+++ lib/mlib.purifier.php
@@ -19,7 +19,7 @@ class MPurifier {
 				$config->set('Attr.EnableID', true);
 				$config->set( 'URI.SafeIframeRegexp', '%^(https?:)?//(www\.youtube(?:-nocookie)?\.com/embed/|player\.vimeo\.com/video/)%' );
 				//$config->set( 'HTML.Allowed', 'textarea[id|cols|rows|class]');
-				$config->set('HTML.AllowedElements', array('textarea', 'div', 'hr', 'br', 'img', 'p'));
+				$config->set('HTML.AllowedElements', array('textarea', 'ul', 'ol', 'li', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'a', 'span', '', 'div', 'hr', 'br', 'img', 'p'));
 				$config->set('HTML.Trusted', true);
 				self::$purifier = new HTMLPurifier( $config );
 			}
