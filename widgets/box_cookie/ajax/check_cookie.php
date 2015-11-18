e457561964adab5209dcc87ce73af60efe057e78
Laca <perjeslaszlo@gmail.com>
2015-11-18 15:54:52 +0100

    Cookie widget

---
+++ widgets/box_cookie/ajax/check_cookie.php
@@ -0,0 +1,16 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: Laca
+	 * Date: 2015.11.18.
+	 * Time: 13:36
+	 */
+
+	error_reporting( E_ALL );
+	ini_set( 'display_errors', 0 );
+
+	define( 'DACCESS', 1 );
+
+	setcookie("accepted", 'YES', time()+(86400 * 1), '/');
+
+	?>
---
+++ widgets/box_cookie/box_cookie.php
@@ -0,0 +1,56 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: Laca
+	 * Date: 2015.11.18.
+	 * Time: 10:43
+	 */
+
+	defined('DACCESS') or die;
+
+	function mwidget_box_cookie() {
+
+		if (isset($_COOKIE['accepted'])) {
+			if ($_COOKIE['accepted'] == 'YES') {
+				return;
+			}
+		}
+
+		$lang = new M_Language;
+		$language = $lang->getLanguage();
+
+		$CookieContent = ORM::for_table('content_meta')->select_many('external_id', 'value')->where('name', 'cookie-box')->find_one();
+
+		if ($CookieContent) {
+
+			?>
+
+			<div class="cookie-alert">
+				<?PHP echo $CookieContent['value']; ?>
+				<p>
+				<a href="index.php?module=content&object=<?PHP echo $CookieContent['external_id']; ?>">Read more...</a>
+				</p>
+				<div class="cookie_button" id="Cookie_Button_OK">OK</div>
+			</div>
+
+			<script>
+
+				$(document).ready(function() {
+					$("#Cookie_Button_OK").click(function() {
+						$(".cookie-alert").css("display", 'none');
+
+						var pathname = 'widgets/box_cookie/ajax/';
+
+						$.ajax({
+							type: 'POST',
+							url: pathname + 'check_cookie.php'
+						});
+
+					});
+				});
+
+			</script>
+
+			<?PHP
+		}
+	}
