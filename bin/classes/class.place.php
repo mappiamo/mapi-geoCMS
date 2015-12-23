ab2794107842c6e6942f6dd8d8616dd43c613be8
Laca <perjeslaszlo@gmail.com>
2015-12-23 11:49:49 +0100

    Failure corrections on Post and Place classes

--- bin/classes/class.place.php
+++ bin/classes/class.place.php
@@ -1,14 +1,33 @@
 <?php
 
-class M_Place extends M_Post {
-	
+	class M_Place extends M_Post {
+
 		protected $type = 'place';
 		protected $kind;
+		protected $start = NULL;
+		protected $end = NULL;
 
-		public function __construct( $id = null ) {
-				if ( $id ) $this->read( $id );
+		public function __construct($id = NULL) {
+			if ($id) {
+				$this->read($id);
+			}
 		}
 
-}
+		public function get_start() {
+			return $this->start;
+		}
+
+		public function get_end() {
+			return $this->end;
+		}
+
+		public function set_start($value) {
+			$this->start = strval($value);
+		}
+
+		public function set_end($value) {
+			$this->end = strval($value);
+		}
+	}
 
 ?>
